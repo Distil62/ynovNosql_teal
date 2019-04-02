67ddf19a408835e6eea63e0fc73741d993e5d327
ImMyst/Titouan.G <titouan.galvani@laposte.net>
2019-04-02 12:29:56 +0200

    DrawerContent detail

--- components/DrawerContent.js
+++ components/DrawerContent.js
@@ -1,17 +1,36 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBicycle } from '@fortawesome/free-solid-svg-icons';
+import { faBicycle, faParking } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-library.add(faBicycle);
+library.add(faBicycle, faParking);
 
-export default props => <div style={{fontSize: '25px'}}>
-    <h3 
-    style={{marginBottom: '10px'}}>
-    <FontAwesomeIcon style={{textAlign: 'middle', marginRight: '10px'}} icon="bicycle"/>
-    </h3>
-    <h3 
-    style={{marginBottom: '10px'}}>
-    <FontAwesomeIcon style={{textAlign: 'middle', marginRight: '10px'}} icon="bicycle"/>13
-    </h3>
-    <h3><FontAwesomeIcon style={{textAlign: 'middle', marginRight: '10px'}} icon="bicycle"/>13</h3>
-</div>
+export default props => 
+<ul style={{fontSize: '15px'}}>
+    <li style={{marginBottom: '25px'}}>
+       <h2 style={{fontSize: '18px'}}><span>{props.velov.properties.commune}</span></h2>
+    </li>
+    <li style={{marginBottom: '10px'}}>
+        <h3
+        style={(props.velov.properties.bike_stands) === 0 ? {color: "#f5222d"} : {color: "#52c41a"}}>
+            <FontAwesomeIcon style={{marginRight: '15px'}} icon="parking"/>
+            totales: <span style={{fontWeight: 'bold'}}>{props.velov.properties.bike_stands}</span>
+        </h3>
+    </li>
+    <li style={{marginBottom: '10px'}}>
+        <h3
+        style={(props.velov.properties.available_bike_stands) === 0 ? {color: "#f5222d"} : {color: "#52c41a"}}>
+            <FontAwesomeIcon style={{marginRight: '15px'}} icon="parking"/>
+            disponibles: <span style={{fontWeight: 'bold'}}>{props.velov.properties.available_bike_stands}</span>
+        </h3>
+    </li>
+    <li style={{marginBottom: '200px'}}>
+        <h3
+        style={(props.velov.properties.available_bikes) === 0 ? {color: "#f5222d"} : {color: "#52c41a"}}>
+            <FontAwesomeIcon style={{marginRight: '13px', marginLeft: '-5px'}} icon="bicycle"/>
+            disponibles: <span style={{fontWeight: 'bold'}}>{props.velov.properties.available_bikes}</span>
+        </h3>
+    </li>
+    <li>
+        <h3>Elle se trouve à : 25m</h3>
+    </li>
+</ul>
