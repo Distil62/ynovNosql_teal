084765774b32ea6e46590c3e9c9f73cef99544b5
ImMyst/Titouan.G <titouan.galvani@laposte.net>
2019-04-02 11:01:30 +0200

    Add icon

--- components/DrawerContent.js
+++ components/DrawerContent.js
@@ -1,4 +1,17 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBicycle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default props => <div>
-    <h1>{}</h1>
+library.add(faBicycle);
+
+export default props => <div style={{fontSize: '25px'}}>
+    <h3 
+    style={{marginBottom: '10px'}}>
+    <FontAwesomeIcon style={{textAlign: 'middle', marginRight: '10px'}} icon="bicycle"/>
+    </h3>
+    <h3 
+    style={{marginBottom: '10px'}}>
+    <FontAwesomeIcon style={{textAlign: 'middle', marginRight: '10px'}} icon="bicycle"/>13
+    </h3>
+    <h3><FontAwesomeIcon style={{textAlign: 'middle', marginRight: '10px'}} icon="bicycle"/>13</h3>
 </div>
